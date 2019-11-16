58452885971902b372c43d3e8820814c4e57a8ab
Chantee Smith <chantee.smith@gmail.com>
2019-11-15 22:37:14 -0600

    Update to list group, and comments

--- comments/app.js
+++ comments/app.js
@@ -1,19 +1,56 @@
 
 let ProfilePic = () => {
-    return  <div className="green">
+    return  <div>
        <img src="drogo.JPG" />
         </div>
 }
 
+
 let CommentBody = () => {
-    return  <div className="green"><b><h1>Khal Drogo</h1></b>
-    <p>Khal Drogo, khal of pups.</p>
-    </div>
+    return  <div>
+        <div className="title">Khal Drogo</div>
+    <div className="subtitle">Khal Drogo, khal of pups.</div>
+    <br />
+    <div className="comments">
+    <div className="commentBox">
+        <b>Dog Sitter 1</b>
+        <p>What a good pup! Very playful and loves attention.</p>
+        </div>
+
+     <div className="commentBox">
+     <b>Dog Sitter 2</b>
+     <p>What a good pup! Loves his food, but is very picky.</p>
+     </div>
+
+     <div className="commentBox">
+     <b>Dog Sitter 3</b>
+     <p>What a good pup! Very playful and loves attention</p>
+     </div>
+     </div>
+     <br />
+     <div className="comments">
+    <div className="commentBox">
+        <b>Dog Sitter 4</b>
+        <p>What a good pup! Very playful and loves attention.</p>
+        </div>
+
+     <div className="commentBox">
+     <b>Dog Sitter 5</b>
+     <p>What a good pup! Loves his food, but is very picky.</p>
+     </div>
+
+     <div className="commentBox">
+     <b>Dog Sitter 6</b>
+     <p>What a good pup! Very playful and loves attention</p>
+     </div>
+     </div>
+ </div>
     // <span>Make this component render a username in bold above the comment text</span>
 }
 
 let Comment = () => {
-    return <div><ProfilePic /><CommentBody /></div>
+    return <div className="background">
+        <ProfilePic /><CommentBody /></div>
     // <span>Make each comment render a profile picture and the comment body</span>
 
 
--- list-group/app.js
+++ list-group/app.js
@@ -1,9 +1,61 @@
 let ListGroupItem = () => {
-    return <span>Make this component render like a Bootstrap list group item: <a href="https://getbootstrap.com/docs/4.1/components/list-group/" target="_blank">https://goo.gl/images/mY5Qdv</a></span>
+    // return <span>Make this component render like a Bootstrap list group item: <a href="https://getbootstrap.com/docs/4.1/components/list-group/" target="_blank">https://goo.gl/images/mY5Qdv</a></span>
+    return <div className="space">
+        <h1>Tomato Soup</h1>
+        <div className="ingredients">
+        <h3>Ingredients</h3>
+        <li>Tomatos</li>
+        <li>Milk</li>
+        <li>Salt</li>
+        <li>Pepper</li>
+        </div>
+        <br />
+        <br />
+       </div>
+
+}
+
+let ListGroupItem2 = () => {
+    return <div>
+        <h1>Mashed Potatoes</h1>
+        <div className="ingredients">
+        <h3>Ingredients</h3>
+        <li>Potatoes</li>
+        <li>Garlic</li>
+        <li>Water</li>
+        <li>Pepper/Salt</li>
+        </div>
+       </div>
+
+}
+let ListGroupItem3 = () => {
+    return <div>
+        <h1>Broccoli and Cheese Casserole</h1>
+        <div className="ingredients">
+        <h3>Ingredients</h3>
+        <li>Cheddar</li>
+        <li>Broccoli</li>
+        <li>Salt</li>
+        <li>Pepper</li>
+        </div>
+       </div>
+
 }
 
 let ListGroup = () => {
-    return <span>Make this Component render a handful of ListGroupItems</span>
+    return <div className="container">
+        <div className="rightSide">
+        <h1 className="header1">Vegetable Recipes</h1>
+        <div className="order">
+        <ListGroupItem /><ListGroupItem2 />
+        </div>
+        <br />
+        <div className="order2">
+        <ListGroupItem3 />
+        </div>
+        </div>
+        
+    </div>
 }
 
 
